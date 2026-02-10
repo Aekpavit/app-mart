499e6a96f09e698aa493e22846a479a1d19d8c12
wanwanat <gomo372g@gmail.com>
2026-02-10 09:59:08 +0700

    frist commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+      <footer style={styles.footer}>
+        <p>© 2026 MyShop. All rights reserved.</p>
+      </footer>
+    )
+  }
+  
+  const styles = {
+    footer: {
+      marginTop: '40px',
+      padding: '16px',
+      textAlign: 'center',
+      background: '#f1f1f1'
+    }
+  }
+  
+  export default Footer
+  
